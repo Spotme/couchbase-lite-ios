5287a52ba7386c9dc40e99077ea9685b3485e2aa
Igor Ievsiukov <igor.ievsiukov@gmail.com>
2015-06-17 17:57:43 +0200

    new MAPBLOCK signature; using JSContext getter instead of _shared lookup

--- Source/API/CBLView.h
+++ Source/API/CBLView.h
@@ -16,7 +16,7 @@ typedef void (^CBLMapEmitBlock)(id key, id value);
 /** A "map" function called when a document is to be added to a view.
     @param doc  The contents of the document being analyzed.
     @param emit  A block to be called to add a key/value pair to the view. Your block can call it zero, one or multiple times. */
-typedef void (^CBLMapBlock)(NSDictionary* doc, CBLMapEmitBlock emit);
+typedef void (^CBLMapBlock)(id doc, CBLMapEmitBlock emit);
 
 /** A "reduce" function called to summarize the results of a view.
 	@param keys  An array of keys to be reduced (or nil if this is a rereduce).
